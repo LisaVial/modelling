4af3d35d389b383eb058fec1e83b42cb745146a1
LisaVial <lisa-ruth.vial@student.uni-tuebingen.de>
2020-10-05 11:00:35 +0200

    brian2: my model does not work so far, i am going to make the examples first

--- brian_test.py
+++ brian_test.py
@@ -39,57 +39,81 @@ g_KL = 0.05 * mS/cm2
 g_NaL = 0.0015 * mS/cm2
 
 
-eqs = '''
+eqs = Equations('''
 # membrane equation with 
-C*dv/dt = J_E - I_L - I_K - I_Na - I_Nap - I_AHP
-dn/dt = phi * (alpha_n * (1 - n) - beta_n * n)
-dh/dt = phi * (alpha_h * (1 - h) - beta_h * h)
-dCa/dt = -epsilon * g_Ca * m_inf1 * (v - E_Ca) - (Ca/tau_Ca)
+dv/dt = J_E - I_L - I_K - I_Na - I_Nap - I_AHP: volt
+dn/dt = phi * (alpha_n * (1 - n) - beta_n * n): 1
+dh/dt = phi * (alpha_h * (1 - h) - beta_h * h): 1
+dCa/dt = -epsilon * g_Ca * m_inf1 * (v - E_Ca) - (Ca/tau_Ca): 1
 
 # current equations
-I_L = g_L (v - E_L)
-I_K = g_K * n ** 4 * (v - E_K)
-I_Na = g_Na * m ** 3 * h * (v - E_Na)
-I_Nap = g_p * m ** 3 * (v - E_Na)
-I_AHP = g_AHP * (Ca / Ca + 1) * (v - E_K)
-m_inf = alpha_m / (alpha_m + beta_m)
-alpha_m = 0.32 * (v + 54)/(1 - exp(-(v+54)/4)
-beta_m = 0.28 * (v + 27)/(exp(v+27/5) - 1)
-alpha_h = 0.128 * exp(-(v+50)/18)
-beta_h = 4/(1 + exp(-(v +27)/5))
-alpha_n = 0.032 * (v + 52) / (1 - exp(-(v + 52)/5)
-m_inf1 = 1 / (1 + exp(-(v+25)/2.5))
-beta_n = 0.5 * exp(-(v+57)/40)
+I_L = g_L (v - E_L): amp
+I_K = g_K * n ** 4 * (v - E_K): amp
+I_Na = g_Na * m ** 3 * h * (v - E_Na): amp
+I_Nap = g_p * m ** 3 * (v - E_Na): amp
+I_AHP = g_AHP * (Ca / Ca + 1) * (v - E_K): amp
+m_inf = alpha_m / (alpha_m + beta_m): Hz
+alpha_m = 0.32 * (v + 54)/(1 - exp(-(v+54)/4): Hz
+beta_m = 0.28 * (v + 27)/(exp(v+27/5) - 1): Hz
+alpha_h = 0.128 * exp(-(v+50)/18): Hz
+beta_h = 4/(1 + exp(-(v +27)/5)): Hz
+alpha_n = 0.032 * (v + 52) / (1 - exp(-(v + 52)/5): Hz
+m_inf1 = 1 / (1 + exp(-(v+25)/2.5)): Hz
+beta_n = 0.5 * exp(-(v+57)/40): Hz
 
 # evolution equations for ionic currents
 d[K]o/dt = 1/tau * (gamma * beta * (I_K + I_AHP + I_KL - 2 * I_pump) + beta * (I_KCC + INKCC) - I_sink + gamma_i * beta 
-            * I_Ki)
-d[Na]i/dt = 1/tau * (-gamma * (I_Na + I_Nap + I_NaL + 3*I_pump) - I_NKCC)
-d[Cl]i/dt = 1/tau * (gamma * (I_GABA + I_ClL) - I_KCC - 2*I_NKCC) 
+            * I_Ki): mM
+d[Na]i/dt = 1/tau * (-gamma * (I_Na + I_Nap + I_NaL + 3*I_pump) - I_NKCC): mM
+d[Cl]i/dt = 1/tau * (gamma * (I_GABA + I_ClL) - I_KCC - 2*I_NKCC): mM 
 
 # co transporters
-I_KCC = rho_KCC * log(([K]i*[Cl]i)/([K]o*[Cl]o))
-I_NKCC = rho_NKCC * f_Ko * (log(([K]i*[Cl]i)/([K]o*[Cl]o)) + log(([Na]i*[Cl]i)/([Na]o*[Cl]o)))
-f_Ko = 1 / (1 + exp(16-[K]o)
+I_KCC = rho_KCC * log(([K]i*[Cl]i)/([K]o*[Cl]o)): mM
+I_NKCC = rho_NKCC * f_Ko * (log(([K]i*[Cl]i)/([K]o*[Cl]o)) + log(([Na]i*[Cl]i)/([Na]o*[Cl]o))): mM
+f_Ko = 1 / (1 + exp(16-[K]o): mM
 
 # sodium potassium pump
-I_pump = rho_pump * (1/gamma) * (1/(1+exp((Na_sat-[Na]i)/3) * ( 1/ (1 + exp(K_sat - [K]o)))
+I_pump = rho_pump * (1/gamma) * (1/(1+exp((Na_sat-[Na]i)/3) * ( 1/ (1 + exp(K_sat - [K]o))): amp
 
 # potassium sink
-I_sink = epsilon_K * ([K]o - K_bath)
+I_sink = epsilon_K * ([K]o - K_bath): amp
 
 # dynamics of complementary ion concentrations
-d[K]i/dt = - 1/tau * (gamma * (I_K + I_AHP + I_KL - 2 * I_pump) + I_KCC + I_NKCC)
-d/dt * (beta * [Na]i + [Na]o) = d/dt * (beta * [Cl]i + [Cl]o) = 0
+d[K]i/dt = - 1/tau * (gamma * (I_K + I_AHP + I_KL - 2 * I_pump) + I_KCC + I_NKCC): mM
+d/dt * (beta * [Na]i + [Na]o) = d/dt * (beta * [Cl]i + [Cl]o) = 0: mM
 
 # reversal potentials
-E_K = (RT/F) * log([K]o/[K]i)
-E_Na = (RT/F) * log([Na]o/[Na]i)
-E_Cl = (RT/F) * log([Cl]o/[Cl]i)
+E_K = (RT/F) * log([K]o/[K]i): mV
+E_Na = (RT/F) * log([Na]o/[Na]i): mV
+E_Cl = (RT/F) * log([Cl]o/[Cl]i): mV
 
 # leak current
-I_L = g_L * (v - E_L)
-g_L = (g_NaL + g_KL + g_ClL)
-E_L = 1/g_L * (g_NaL * E_Na + g_KL * E_K + g_ClL * E_Cl)
+I_L = g_L * (v - E_L): amp
+g_L = (g_NaL + g_KL + g_ClL): 1
+E_L = 1/g_L * (g_NaL * E_Na + g_KL * E_K + g_ClL * E_Cl): mV
 
-'''
+''')
+
+P = NeuronGroup(4000, model=eqs, threshold='v>-20*mV', refractory=3*ms,
+                method='exponential_euler')
+Pe = P[:3200]
+Pi = P[3200:]
+Ce = Synapses(Pe, P, on_pre='ge+=we')
+Ci = Synapses(Pi, P, on_pre='gi+=wi')
+Ce.connect(p=0.02)
+Ci.connect(p=0.02)
+
+# Initialization
+P.v = 'El + (randn() * 5 - 5)*mV'
+P.ge = '(randn() * 1.5 + 4) * 10.*nS'
+P.gi = '(randn() * 12 + 20) * 10.*nS'
+
+# Record a few traces
+trace = StateMonitor(P, 'v', record=[1, 10, 100])
+run(1 * second, report='text')
+plot(trace.t/ms, trace[1].v/mV)
+plot(trace.t/ms, trace[10].v/mV)
+plot(trace.t/ms, trace[100].v/mV)
+xlabel('t (ms)')
+ylabel('v (mV)')
+show()
