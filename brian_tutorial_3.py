94045b68e8c4b5e2b691d94a90d80aed983d0ecb
LisaVial <lisa-ruth.vial@student.uni-tuebingen.de>
2020-10-22 11:32:12 +0200

    completed the brain2 tutorial, after cmos stuff i will check my old code of the Traub-Miles model to see if I can fix it

--- brian_tutorial_3.py
+++ brian_tutorial_3.py
@@ -1,7 +1,8 @@
 from brian2 import *
+from matplotlib.image import imread
 import matplotlib.pyplot as plt
 import numpy as np
-
+from IPython import embed
 
 # Simulations
 # multiple runs
@@ -133,7 +134,7 @@ import numpy as np
 # changing things during a run
 start_scope()
 # parameters
-area = 20000*umetre**2
+area = 20000 * umetre ** 2
 Cm = 1 * ufarad * cm ** -2 * area
 gl = 5e-5 * siemens * cm ** -2 * area
 El = -65 * mV
@@ -143,28 +144,207 @@ g_na = 100 * msiemens * cm ** -2 * area
 g_kd = 30 * msiemens * cm ** -2 * area
 VT = -63 * mV
 # the model
-eqs_HH = '''
-dv/dt = (gl * (El - v) - g_na * (m*m*m) * h * (v - ENa) - g_kd * (n * n *n) * (v - Ek) + I)/cm: volt
-dm/dt = 0.32 * (mV**-1) * (13.*mV-v+VT) / (exp((13.*mV-v+VT)/
-(4 * mV)) - 1)/ms * (1-m)-0.28*(mV**-1)*(v-VT-40*mV)/(exp((v-VT-40*mV)/(5*mV)-1)/ms*m: 1
-dn/dt = 0.032 * (mV **-1)*(15*mV-v+VT)/
-(exp((15*mV-v+VT)/(5*mV))-1)/ms*(1-n)-5*exp((10*mV-v+VT)/(40*mV))/ms*n: 1
-dh/dt = 0.128*exp((17*mV-v+VT)/(18*mV))/ms*(1-h)-4/(1+exp((40*mV-v+VT)/(5*mV)))/ms*h: 1
-I: amp
-'''
-group = NeuronGroup(1, eqs_HH, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
-group.v = El
-statemon = StateMonitor(group, 'v', record=True)
-spikemon = SpikeMonitor(group, variables='v')
+# eqs_HH = '''
+# dv/dt = (gl * (El - v) - g_na * (m * m * m) * h * (v - ENa) - g_kd * (n * n * n * n) * (v - EK) + I)/Cm: volt
+# dm/dt = 0.32 * (mV ** -1) * (13. * mV - v + VT) / (exp((13. * mV - v + VT)/
+# (4. * mV)) - 1.)/ms * (1 - m) - 0.28 * (mV ** -1) * (v - VT - 40.*mV)/(exp((v - VT - 40.*mV)/(5.*mV))-1)/ms*m: 1
+# dn/dt = 0.032 * (mV ** -1) * (15. * mV - v + VT)/
+# (exp((15. * mV - v + VT)/(5. * mV)) - 1.) / ms * (1. - n) - .5 * exp((10. * mV - v + VT)/(40.*mV))/ms*n: 1
+# dh/dt = 0.128 * exp((17. * mV - v + VT)/(18. * mV))/ms * (1. - h) - 4./ (1+exp((40. * mV - v + VT)/
+# (5. * mV)))/ms*h: 1
+# I: amp
+# '''
+# group = NeuronGroup(1, eqs_HH, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
+# group.v = El
+# statemon = StateMonitor(group, 'v', record=True)
+# spikemon = SpikeMonitor(group, variables='v')
+#
+# plt.figure(figsize=(9, 4))
+# for l in range(5):
+#     group.I = rand() * 50 * nA
+#     run(10 * ms)
+#     plt.axvline(l*10, ls='--', c='k')
+# plt.axhline(El/mV, ls='-', c='lightgray', lw=3)
+# plt.plot(statemon.t/ms, statemon.v[0]/mV, '-b')
+# plt.plot(spikemon.t/ms, spikemon.v/mV, 'ob')
+# plt.xlabel('time [ms]')
+# plt.ylabel('v [mV]')
+# plt.show()
+#
+# # my network above behaves really strange in comparision to what is shown in the tutorial
+#
+# start_scope()
+#
+# group = NeuronGroup(1, eqs_HH, threshold='v > -40 * mV', refractory='v > -40 * mV', method='exponential_euler')
+# group.v = El
+#
+# statemon = StateMonitor(group, 'v', record=True)
+# spikemon = SpikeMonitor(group, variables='v')
+#
+# # we replace the loop from above with a run_regularly
+# group.run_regularly('I = rand() * 50 * nA', dt = 10*ms)
+# run(50*ms)
+# plt.figure(figsize=(9,4))
+# # we keep the loop just to draw vertical lines
+# for l in range(5):
+#     plt.axvline(l*10, ls='--', c='k')
+# plt.axhline(El/mV, ls='-', c='lightgray', lw=3)
+# plt.plot(statemon.t/ms, statemon.v[0]/mV, '-b')
+# plt.plot(spikemon.t/ms, spikemon.v/mV, 'ob')
+# plt.xlabel('time [ms]')
+# plt.ylabel('V [mV]')
+# plt.show()
+#
+# start_scope()
+#
+# group = NeuronGroup(1, eqs_HH, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
+# group.v = El
+#
+# statemon = StateMonitor(group, 'v', record=True)
+# spikemon = SpikeMonitor(group, variables='v')
+#
+# @network_operation(dt=10*ms)
+# def change_I():
+#     group.I = rand()*50*nA
+# run(50*ms)
+# plt.figure(figsize=(9, 4))
+# for l in range(5):
+#     plt.axvline(l*10, ls='--', c='k')
+# plt.axhline(El/mV, ls='-', c='lightgray', lw=3)
+# plt.plot(statemon.t/ms, statemon.v[0]/mV, '-b')
+# plt.plot(spikemon.t/ms, spikemon.v/mV, 'ob')
+# plt.xlabel('time [ms]')
+# plt.ylabel('v [mv]')
+# plt.show()
 
-plt.figure(figsize=(9, 4))
-for l in range(5):
-    group.I = rand() * 50 * nA
-    run(10 * ms)
-    plt.axvline(l*10, ls='--', c='k')
-plt.axhline(El/mV, ls='-', c='lightgray', lw=3)
-plt.plot(statemon.t/ms, statemon.v[0]/mV, '-b')
-plt.plot(statemon.t/ms, statemon.v/mV, 'ob')
-plt.xlabel('time [ms]')
-plt.ylabel('v [mV]')
-plt.show()
+# multiple neurons
+# start_scope()
+# N = 3
+# eqs_HH_2 = '''
+# dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/C : volt
+# dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/
+#     (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
+#     (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
+# dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/
+#     (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+# dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+# I : amp
+# C : farad
+# '''
+# group = NeuronGroup(N, eqs_HH_2, threshold='v > -40*mV', refractory='v > -40*mV', method='exponential_euler')
+# group.v = El
+# # initialize with some different capacitances
+# group.C = array([0.8, 1, 1.3])*ufarad*cm**-2*area
+#
+# statemon = StateMonitor(group, variables=True, record=True)
+# group.run_regularly('I = rand()*50*nA', dt=10*ms)
+# run(50*ms)
+#
+# plt.figure(figsize=(9, 4))
+# plt.subplot(121)
+# for l in range(5):
+#     plt.axvline(l*10, ls='--', c='k')
+# plt.axhline(El/mV, ls='-', c='lightgray', lw=3)
+# plt.plot(statemon.t/ms, statemon.v.T/mV, '-')
+# plt.xlabel('time [ms]')
+# plt.ylabel('v [mV]')
+# plt.subplot(122)
+# plt.plot(statemon.t/ms, statemon.I.T/nA, '-')
+# plt.xlabel('time [ms]')
+# plt.ylabel('I [nA]')
+# plt.show()
+#
+# start_scope()
+# N = 3
+# eqs_HH_3 = '''
+# dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/C : volt
+# dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/
+#     (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
+#     (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
+# dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/
+#     (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+# dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+# I : amp (shared) # everything is the same except we've added this shared
+# C : farad
+# '''
+# group = NeuronGroup(N, eqs_HH_3,
+#                     threshold='v > -40*mV',
+#                     refractory='v > -40*mV',
+#                     method='exponential_euler')
+# group.v = El
+# group.C = array([0.8, 1, 1.2]) * ufarad * cm ** -2 * area
+# statemon = StateMonitor(group, 'v', record=True)
+# group.run_regularly('I = rand()*50*nA', dt=10*ms)
+# run(50*ms)
+# plt.figure(figsize=(9, 4))
+# for l in range(5):
+#     plt.axvline(l*10, ls='--', c='k')
+# plt.axhline(El/mV, ls='-', c='lightgray', lw=3)
+# plt.plot(statemon.t/ms, statemon.v.T/mV, '-')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('v (mV)')
+# plt.show()
+
+# adding input
+# A = 2.5
+# f = 10*Hz
+# tau = 5*ms
+# # create a TimedArray and set the equations to use it
+# t_recorded = arange(int(200*ms/defaultclock.dt))*defaultclock.dt
+# I_recorded = TimedArray(A*sin(2*pi*f*t_recorded), dt=defaultclock.dt)
+# eqs = '''
+# dv/dt = (I - v)/tau: 1
+# I = I_recorded(t): 1
+# '''
+# G = NeuronGroup(1, eqs, threshold='v > 1', reset='v = 0', method='exact')
+# M = StateMonitor(G, variables=True, record=True)
+# run(200*ms)
+# plt.plot(M.t/ms, M.v[0], label='v')
+# plt.plot(M.t/ms, M.I[0], label='I')
+# plt.xlabel('time [ms]')
+# plt.ylabel('v')
+# plt.legend(loc='best')
+# plt.show()
+
+# create TimedArray that could not be reproduced with a formula
+# A = 2.5
+# f = 10*Hz
+# tau = 5*ms
+# # create a TimedArray and set the equations to use it
+# num_samples = int(200*ms/defaultclock.dt)
+# I_arr = zeros(num_samples)
+# for _ in range(100):
+#     a = randint(num_samples)
+#     I_arr[a:a+100] = rand()
+# I_recorded = TimedArray(A*I_arr, dt=defaultclock.dt)
+# eqs = '''
+# dv/dt = (I - v)/tau: 1
+# I = I_recorded(t): 1
+# '''
+# G = NeuronGroup(1, eqs, threshold='v > 1', reset='v = 0', method='exact')
+# M = StateMonitor(G, variables=True, record=True)
+# run(200*ms)
+# plt.plot(M.t/ms, M.v[0], label='v')
+# plt.plot(M.t/ms, M.I[0], label='I')
+# plt.xlabel('time [ms]')
+# plt.ylabel('v')
+# plt.legend(loc='best')
+# plt.show()
+
+start_scope()
+img = (1-imread('brian.png'))[::-1, :, 0].T
+num_samples, N = img.shape
+ta = TimedArray(img, dt=1*ms) # 228
+A = 1.5
+tau = 2*ms
+eqs = '''
+dv/dt = (A*ta(t, i)-v)/tau+0.8*xi*tau**-0.5 : 1
+'''
+G = NeuronGroup(N, eqs, threshold='v>1', reset='v=0', method='euler')
+M = SpikeMonitor(G)
+run(num_samples*ms)
+plot(M.t/ms, M.i, '.k', ms=3)
+xlim(0, num_samples)
+ylim(0, N)
+xlabel('Time (ms)')
+ylabel('Neuron index');
